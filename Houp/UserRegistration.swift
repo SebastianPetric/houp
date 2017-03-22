39ed495d1c5b884175b2ae2e9b337da2de019313
MacbookPro <macbookpro@sebastian-mac.hfu-open.fu.hs-furtwangen.de>
2017-03-22 18:59:56 +0100

    Cleaned Code

---
+++ Houp/UserRegistration.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  Houp
+//
+//  Created by Sebastian on 22.03.17.
+//  Copyright © 2017 SP. All rights reserved.
+//
+
+import Foundation
+
+struct UserRegistration{
+    let username: String?
+    let prename: String?
+    let name: String?
+    let email: String?
+    let password: String?
+    let profileImage: UIImage?
+}
